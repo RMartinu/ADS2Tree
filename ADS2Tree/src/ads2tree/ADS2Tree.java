780c2523893aee51f36de44675d7c9995503821f
Robert Martinu <robert.s.martinu@gmail.com>
2018-04-12 03:57:10 +0200

    some more Functions, 3/4 tested, Formatting the Printing

--- ADS2Tree/src/ads2tree/ADS2Tree.java
+++ ADS2Tree/src/ads2tree/ADS2Tree.java
@@ -5,7 +5,12 @@
  */
 package ads2tree;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Locale;
 import java.util.Scanner;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 /**
  *
@@ -19,21 +24,48 @@ public class ADS2Tree {
     public static void main(String[] args) {
         // TODO code application logic here
        // Node TreeToTest=BuildTree(args[0]);
+       if(args.length<1)
+       {
+           System.err.println("Please specifiy file");
+           return;
+       }
+       System.out.println("Parsin: "+args[0]);
         TreeTester TestThisTree=new TreeTester(BuildTree(args[0]));
+        boolean isAVL=TestThisTree.checkTree();
+        int min, max; 
+        double avg;
+        min=TestThisTree.findMin();
+        max=TestThisTree.findMax();
+        avg=TestThisTree.findAverage();
+        
+        System.out.println("AVL: " + ((isAVL)?"Yes":"No"));
+        //System.out.println("min: " + min + "max: " + max + "avg: "+ avg);
+        
+        System.out.format(Locale.US,"min: %d max: %d avg: %.1f ", min, max, avg);
     }
     
     static Node BuildTree(String filename)
     {
         Node h=null;
-        Scanner sc=new Scanner(filename);
+        File f=new File(filename);
+        Scanner sc=null;
+        try {
+            sc = new Scanner(f);
+        } catch (FileNotFoundException ex) {
+            Logger.getLogger(ADS2Tree.class.getName()).log(Level.SEVERE, null, ex);
+        }
         if(sc.hasNextInt())
+           
         {
+            
             h=new Node(sc.nextInt());
             {
                 while(sc.hasNextInt())
                     h.insert(sc.nextInt());
             }
-        }   
+        } 
+        if(h==null)
+            System.err.println("PArsing Error");
         return h;
     }
     
